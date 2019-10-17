a322c8ac3282123b89e4445daf816650eb12c163
Siddarth Arumugam <sa3418@columbia.edu>
2019-10-17 17:20:37 -0400

    Added landmark files
    Added landmark files

--- hw2/index.js
+++ hw2/index.js
@@ -1,160 +1,183 @@
-
-const HOSTED_URLS = {
-  model:
-      'model_js/model.json',
-  metadata:
-      'model_js/metadata.json'
+CLASSES = {
+  0: 'class0',
+  1: 'class1',
+  2: 'class2',
+  3: 'class3',
+  4: 'class4',
 };
 
-const examples = {
-  'example1':
-      'Alice was beginning to get very tired of sitting by her sister on the bank.',
-  'example2':
-      'Buda-Pesth seems a wonderful place.',
-  'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
-};
+const MODEL_PATH =
+    'model.json';
 
-function status(statusText) {
-  console.log(statusText);
-  document.getElementById('status').textContent = statusText;
-}
+const IMAGE_SIZE = 192;
+const TOPK_PREDICTIONS = 5;
 
-function showMetadata(metadataJSON) {
-  document.getElementById('vocabularySize').textContent =
-      metadataJSON['vocabulary_size'];
-  document.getElementById('maxLen').textContent =
-      metadataJSON['max_len'];
-}
+let my_model;
+const demo = async () => {
+  status('Loading model...');
 
-function settextField(text, predict) {
-  const textField = document.getElementById('text-entry');
-  textField.value = text;
-  doPredict(predict);
-}
+  my_model = await tf.loadLayersModel(MODEL_PATH);
 
-function setPredictFunction(predict) {
-  const textField = document.getElementById('text-entry');
-  textField.addEventListener('input', () => doPredict(predict));
-}
+  // Warmup the model. This isn't necessary, but makes the first prediction
+  // faster. Call `dispose` to release the WebGL memory allocated for the return
+  // value of `predict`.
+  my_model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
 
-function disableLoadModelButtons() {
-  document.getElementById('load-model').style.display = 'none';
-}
+  status('');
 
-function doPredict(predict) {
-  const textField = document.getElementById('text-entry');
-  const result = predict(textField.value);
-  score_string = "Class scores: ";
-  for (var x in result.score) {
-    score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
-  }
-  //console.log(score_string);
-  status(
-      score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
-}
-
-function prepUI(predict) {
-  setPredictFunction(predict);
-  const testExampleSelect = document.getElementById('example-select');
-  testExampleSelect.addEventListener('change', () => {
-    settextField(examples[testExampleSelect.value], predict);
-  });
-  settextField(examples['example1'], predict);
-}
-
-async function urlExists(url) {
-  status('Testing url ' + url);
-  try {
-    const response = await fetch(url, {method: 'HEAD'});
-    return response.ok;
-  } catch (err) {
-    return false;
-  }
-}
-
-async function loadHostedPretrainedModel(url) {
-  status('Loading pretrained model from ' + url);
-  try {
-    const model = await tf.loadLayersModel(url);
-    status('Done loading pretrained model.');
-    disableLoadModelButtons();
-    return model;
-  } catch (err) {
-    console.error(err);
-    status('Loading pretrained model failed.');
-  }
-}
-
-async function loadHostedMetadata(url) {
-  status('Loading metadata from ' + url);
-  try {
-    const metadataJson = await fetch(url);
-    const metadata = await metadataJson.json();
-    status('Done loading metadata.');
-    return metadata;
-  } catch (err) {
-    console.error(err);
-    status('Loading metadata failed.');
-  }
-}
-
-class Classifier {
-
-  async init(urls) {
-    this.urls = urls;
-    this.model = await loadHostedPretrainedModel(urls.model);
-    await this.loadMetadata();
-    return this;
-  }
-
-  async loadMetadata() {
-    const metadata =
-        await loadHostedMetadata(this.urls.metadata);
-    showMetadata(metadata);
-    this.maxLen = metadata['max_len'];
-    console.log('maxLen = ' + this.maxLen);
-    this.wordIndex = metadata['word_index']
-  }
-
-  predict(text) {
-    // Convert to lower case and remove all punctuations.
-    const inputText =
-        text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' ');
-    // Look up word indices.
-    const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
-    for (let i = 0; i < inputText.length; ++i) {
-      const word = inputText[i];
-      inputBuffer.set(this.wordIndex[word], 0, i);
-      //console.log(word, this.wordIndex[word], inputBuffer);
+  // Make a prediction through the locally hosted cat.jpg.
+  const catElement = document.getElementById('cat');
+  if (catElement.complete && catElement.naturalHeight !== 0) {
+    predict(catElement);
+    catElement.style.display = '';
+  } else {
+    catElement.onload = () => {
+      predict(catElement);
+      catElement.style.display = '';
     }
-    const input = inputBuffer.toTensor();
-    //console.log(input);
-
-    status('Running inference');
-    const beginMs = performance.now();
-    const predictOut = this.model.predict(input);
-    //console.log(predictOut.dataSync());
-    const score = predictOut.dataSync();//[0];
-    predictOut.dispose();
-    const endMs = performance.now();
-
-    return {score: score, elapsed: (endMs - beginMs)};
   }
+
+  document.getElementById('file-container').style.display = '';
 };
 
-async function setup() {
-  if (await urlExists(HOSTED_URLS.model)) {
-    status('Model available: ' + HOSTED_URLS.model);
-    const button = document.getElementById('load-model');
-    button.addEventListener('click', async () => {
-      const predictor = await new Classifier().init(HOSTED_URLS);
-      prepUI(x => predictor.predict(x));
-    });
-    button.style.display = 'inline-block';
-  }
+/**
+ * Given an image element, makes a prediction through my_model returning the
+ * probabilities of the top K classes.
+ */
+async function predict(imgElement) {
+  status('Predicting...');
 
-  status('Standing by.');
+  // The first start time includes the time it takes to extract the image
+  // from the HTML and preprocess it, in additon to the predict() call.
+  const startTime1 = performance.now();
+  // The second start time excludes the extraction and preprocessing and
+  // includes only the predict() call.
+  let startTime2;
+  const logits = tf.tidy(() => {
+    // tf.browser.fromPixels() returns a Tensor from an image element.
+    const img = tf.browser.fromPixels(imgElement).toFloat();
+
+    // const offset = tf.scalar(127.5);
+    // Normalize the image from [0, 255] to [-1, 1].
+    // const normalized = img.sub(offset).div(offset);
+    const normalized = img.div(255.0);
+
+    // Reshape to a single-element batch so we can pass it to predict.
+    const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+
+    startTime2 = performance.now();
+    // Make a prediction through my_model.
+    return my_model.predict(batched);
+  });
+
+  // Convert logits to probabilities and class names.
+  const classes = await getTopKClasses(logits, TOPK_PREDICTIONS);
+  const totalTime1 = performance.now() - startTime1;
+  const totalTime2 = performance.now() - startTime2;
+  status(`Done in ${Math.floor(totalTime1)} ms ` +
+      `(not including preprocessing: ${Math.floor(totalTime2)} ms)`);
+
+  // Show the classes in the DOM.
+  showResults(imgElement, classes);
 }
 
-setup();
+/**
+ * Computes the probabilities of the topK classes given logits by computing
+ * softmax to get probabilities and then sorting the probabilities.
+ * @param logits Tensor representing the logits from my_model.
+ * @param topK The number of top predictions to show.
+ */
+async function getTopKClasses(logits, topK) {
+  const values = await logits.data();
+
+  const valuesAndIndices = [];
+  for (let i = 0; i < values.length; i++) {
+    valuesAndIndices.push({value: values[i], index: i});
+  }
+  valuesAndIndices.sort((a, b) => {
+    return b.value - a.value;
+  });
+  const topkValues = new Float32Array(topK);
+  const topkIndices = new Int32Array(topK);
+  for (let i = 0; i < topK; i++) {
+    topkValues[i] = valuesAndIndices[i].value;
+    topkIndices[i] = valuesAndIndices[i].index;
+  }
+
+  const topClassesAndProbs = [];
+  for (let i = 0; i < topkIndices.length; i++) {
+    topClassesAndProbs.push({
+      className: CLASSES[topkIndices[i]],
+      probability: topkValues[i]
+    })
+  }
+  return topClassesAndProbs;
+}
+
+//
+// UI
+//
+
+function showResults(imgElement, classes) {
+  const predictionContainer = document.createElement('div');
+  predictionContainer.className = 'pred-container';
+
+  const imgContainer = document.createElement('div');
+  imgContainer.appendChild(imgElement);
+  predictionContainer.appendChild(imgContainer);
+
+  const probsContainer = document.createElement('div');
+  for (let i = 0; i < classes.length; i++) {
+    const row = document.createElement('div');
+    row.className = 'row';
+
+    const classElement = document.createElement('div');
+    classElement.className = 'cell';
+    classElement.innerText = classes[i].className;
+    row.appendChild(classElement);
+
+    const probsElement = document.createElement('div');
+    probsElement.className = 'cell';
+    probsElement.innerText = classes[i].probability.toFixed(3);
+    row.appendChild(probsElement);
+
+    probsContainer.appendChild(row);
+  }
+  predictionContainer.appendChild(probsContainer);
+
+  predictionsElement.insertBefore(
+      predictionContainer, predictionsElement.firstChild);
+}
+
+const filesElement = document.getElementById('files');
+filesElement.addEventListener('change', evt => {
+  let files = evt.target.files;
+  // Display thumbnails & issue call to predict each image.
+  for (let i = 0, f; f = files[i]; i++) {
+    // Only process image files (skip non image files)
+    if (!f.type.match('image.*')) {
+      continue;
+    }
+    let reader = new FileReader();
+    const idx = i;
+    // Closure to capture the file information.
+    reader.onload = e => {
+      // Fill the image & call predict.
+      let img = document.createElement('img');
+      img.src = e.target.result;
+      img.width = IMAGE_SIZE;
+      img.height = IMAGE_SIZE;
+      img.onload = () => predict(img);
+    };
+
+    // Read in the image file as a data URL.
+    reader.readAsDataURL(f);
+  }
+});
+
+const demoStatusElement = document.getElementById('status');
+const status = msg => demoStatusElement.innerText = msg;
+
+const predictionsElement = document.getElementById('predictions');
+
+demo();
