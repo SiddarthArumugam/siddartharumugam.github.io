fcec17c4282ae5a575aabe45582eacd5aa455789
Siddarth Arumugam <sa3418@columbia.edu>
2019-10-17 17:43:28 -0400

    Update index.js

--- hw2/index.js
+++ hw2/index.js
@@ -3,14 +3,13 @@ CLASSES = {
   1: 'class1',
   2: 'class2',
   3: 'class3',
-  4: 'class4',
 };
 
 const MODEL_PATH =
     'model.json';
 
 const IMAGE_SIZE = 192;
-const TOPK_PREDICTIONS = 5;
+const TOPK_PREDICTIONS = 4;
 
 let my_model;
 const demo = async () => {
